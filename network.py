ed48a2490d6cfd8d53b145add5106cd4feb78fe5
Jindřich Machka <jindra.machka@gmail.com>
2023-01-24 22:10:16 +0100

    Pseudocode for backpropagation, progress on backpropagation, problems with the cost function

--- network.py
+++ network.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 class Network:
     def __init__(self, topology):
         self.activation_functions = {"sigmoid": self.sigmoid_activation}
@@ -10,7 +9,7 @@ class Network:
         for l in range(1, self.L):
             self.biases.append(np.random.randn(self.topology[l], 1))
             self.weights.append(np.random.randn(self.topology[l], self.topology[l-1]))
-
+        
         # self.weights = [None, 
         #                 [[3,  2],
         #                  [6, 2],
@@ -21,7 +20,7 @@ class Network:
         #                 [2],
         #                 [4]], 
         #                [[4]]]
-
+        
         print(self.weights)
         print()
         print(self.biases)
@@ -33,19 +32,48 @@ class Network:
         A = X
         for l in range(1, self.L):
             W = self.weights[l]
+            print("Weights for layer l:")
             print(W)
+
             B = self.biases[l]
+            print("Biases for layer l:")
             print(B)
+
+            print("Activations for layer l-1:")
             print(A)
             Z = np.dot(W, A) + B
+            print("Weighted sums for layer l")
             print(Z)
+
             A = activation_function(Z)
+            print("Activations for layer l:")
+            print(A)
+
+            print()
+            self.neuron_activations.append(A)
+            self.weighted_sums.append(Z)
+
+        print()
+        print("All biases:")
+        print(self.biases)
+        print("All weights:")
+        print(self.weights)
+        print("Weighted sums of all neurons:")
+        print(self.weighted_sums)
+        print("Activations of all neurons: ")
+        print(self.neuron_activations)
 
     def sigmoid_activation(self, Z):
         return 1/(1+np.exp(-Z))
 
+    def sigmoid_derivative(self, z):
+        return self.sigmoid_activation(z)*(1-self.sigmoid_activation(z))
+
     def cost_function(self, h, y):
-        return np.array([sum(j) for j in [(y[i]-h[i])**2 for i in range(len(h))]])/len(h)
+        pass
+
+    def cost_derivative(self, a):
+        pass
 
     def gradient_descent(self, epochs, learning_parameter):
         for e in range(epochs):
@@ -54,20 +82,29 @@ class Network:
             weight_gradient = gradient[0]
             bias_gradient = gradient[1]
 
-
             for l in range(self.L):
                 self.weights[l] -= learning_parameter * weight_gradient[l]
                 self.biases[l] -= learning_parameter * bias_gradient[l]
 
     def backprop(self, cost):
-        for l in range(self.L):
-            pass
+        activation_gradient = []
+        for l in range(self.L-1, -1, -1):
+            p_d_prev = []
+            for j in range(l):
+                a = self.neuron_activations[j]
+                z = self.weighted_sums[j]
+                if j == self.L-1:
+                    dCda = self.cost_derivative(a)
+                    # a = self.neuron_activations[j]
+                    # z = self.weighted_sums[j]
+                    # dCdw = a*self.sigmoid_derivative()*self.cost_derivative(a)
+                    # dCdb = self.sigmoid_derivative()*self.cost_derivative(a)
+                else:
+                    dCda = [self.weights*z for j in range(l)]
+                p_d_prev.append(dCda)
 
 
-
-net = Network((5, 10, 15, 5))
+net = Network((3, 2, 1))
 net.forwardprop("sigmoid", [[3], 
                             [2], 
-                            [5],
-                            [6],
-                            [2]])
+                            [5]])
--- test.py
+++ test.py
@@ -2,28 +2,44 @@ import numpy as np
 import sympy as sp
 
 # Cost function
-h = np.array([[2, 3, 4],
-              [4, 1, 3],
-              [5, 3, 1],
-              [2, 4, 1]])
-y = np.array([[3, 1, 4],
-              [3, 5, 1],
-              [2, 2, 4],
-              [5, 2, 3]])
+
+# 1 - columns as training examples, rows as neurons
+h = np.array([[2, 3, 4],  # 3 training outputs of output neuron 1 (for training examples 1, 2 and 3)
+              [4, 1, 3],  # 3 training outputs of output neuron 2
+              [5, 3, 1],  # 3 training outputs of output neuron 3
+              [2, 4, 1]]) # 3 training outputs of output neuron 4
+y = np.array([[3, 1, 4],  # 3 desired outputs of output neuron 1
+              [3, 5, 1],  # 3 desired outputs of output neuron 2
+              [2, 2, 4],  # 3 desired outputs of output neuron 3
+              [5, 2, 3]]) # 3 desired outputs of output neuron 4
+# OR
+# 2 - columns as neurons, rows as training examples (the correct way)
+h = np.array([[2, 3, 4],  # 1st training output for output neurons 1, 2 and 3
+              [4, 1, 3],  # 2nd training output for output neurons 1, 2 and 3
+              [5, 3, 1],  # 3rd training output for output neurons 1, 2 and 3
+              [2, 4, 1]]) # 4th training output for output neurons 1, 2 and 3
+y = np.array([[3, 1, 4],  # 1st desired output for output neurons 1, 2 and 3
+              [3, 5, 1],  # 2nd desired output for output neurons 1, 2 and 3
+              [2, 2, 4],  # 3rd desired output for output neurons 1, 2 and 3
+              [5, 2, 3]]) # 4th desired output for output neurons 1, 2 and 3
 c=[]
 for i in range(len(h)):
     c.append((y[i]-h[i])**2)
 print(c)
-print([sum(j) for j in [(y[i]-h[i])**2 for i in range(len(h))]])
-c = np.array([sum(j) for j in [(y[i]-h[i])**2 for i in range(len(h))]])/len(h)
+print([(y[i]-h[i])**2 for i in range(len(h))]) 
+print([sum(j) for j in [(y[i]-h[i])**2 for i in range(len(h))]]) # A list of four costs - 1 for each training example
+c = sum([sum(j) for j in [(y[i]-h[i])**2 for i in range(len(h))]])/len(h)
 print(c)
 
-print(np.subtract(y, h))
-print(np.square(np.subtract(y, h)))
-print(np.square(np.subtract(y, h)).mean())
+# print(np.subtract(y, h))
+# print(np.square(np.subtract(y, h)))
+# print(np.square(np.subtract(y, h)).mean())
 
 # Derivative of the cost
 # x1, y1, z1, a1, b1, c1 = sp.symbols("x1 y1 y1 a1 b1 c1")
 # dc = sp.Derivative((sp.Vector(([x1, y1, z1]) - np.array([a1, b1, c1]))**2)/5, x1)
 # print(f"{dc}")
 # print(f"{dc.doit()}")
+
+for l in range(15-1, 0-1, -1):
+    print(l)
