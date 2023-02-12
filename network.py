a69c35b8736b40f9693e04e7dca5441fe25ca716
Jindřich Machka <jindra.machka@gmail.com>
2023-02-12 17:59:40 +0100

    Adjusted the code for training the ANN; adjusted variable names

--- network.py
+++ network.py
@@ -18,19 +18,93 @@ class Network:
         for l in range(1, self.L):
             self.biases.append(np.random.randn(self.topology[l], 1))
             self.weights.append(np.random.randn(self.topology[l], self.topology[l-1]))
+
+    def stochastic_gradient_descent(self, activation_functions, X_train, y_train, epochs, mini_batch_size, learning_rate):
+
+        self.activation_functions = [None if func == None else self.activation_functions[func] for func in activation_functions]
+
+        for e in range(epochs):
+            training_data = list(zip(X_train, y_train))
+            shuffle(training_data)
+            h = None
+    
+            mini_batches = []
+            for mb in range(0, len(training_data), mini_batch_size):
+                mini_batches.append(training_data[mb:mb+mini_batch_size])
+
+            for mini_batch in mini_batches:
+                for x, y in mini_batch:
+                    self.forward_propagation(x)
+                    h = self.neuron_activations[self.L-1].T[0]
+                    nabla_wb_x = self.backward_propagation(h, y)
+                    nabla_w_x = nabla_wb_x[0]
+                    nabla_b_x = nabla_wb_x[1]
+
+                    for l in range(1, self.L):
+                        self.weights[l] -= learning_rate * nabla_w_x[l]/len(mini_batch)
+                        self.biases[l] -= learning_rate * nabla_b_x[l]/len(mini_batch)
+
+            hs = []
+            for x, y in zip(X_train, y_train):
+                self.forward_propagation(x)
+                h = self.neuron_activations[self.L-1].T[0]
+                hs.append(h)
+            predictions = np.array(hs).argmax(axis=1)
+            target_values = y_train.argmax(axis=1)
+            accuracy = sum([1 for i in range(len(hs)) if predictions[i] == target_values[i]])/len(hs)
+            cost = self.cost_function(hs, y_train)
+
+            print(f"Epoch {e} -> Cost: {cost}, Accuracy: {accuracy}")
             
-    def forwardprop(self, X):
-        self.neuron_activations = [X]
+    def forward_propagation(self, x):
+        self.neuron_activations = [x]
         self.weighted_sums = [None]
-        A = X
+        current_layer_A = x
         for l in range(1, self.L):
             activation_function = self.activation_functions[l][0]
-            W = self.weights[l]
-            B = self.biases[l]
-            Z = np.dot(W, A) + B
-            A = activation_function(Z)
-            self.neuron_activations.append(A)
-            self.weighted_sums.append(Z)
+
+            current_layer_W = self.weights[l]
+            previous_layer_A = current_layer_A
+            current_layer_B = self.biases[l]
+
+            current_layer_Z = np.dot(current_layer_W, previous_layer_A) + current_layer_B
+            current_layer_A = activation_function(current_layer_Z)
+
+            self.neuron_activations.append(current_layer_A)
+            self.weighted_sums.append(current_layer_Z)
+
+    def backward_propagation(self, h, y):
+        nabla_a = []
+        last_layer_nabla_a = np.array([[self.cost_derivative(hi, yi)] for hi, yi in zip(h, y)])
+
+        nabla_a.append(last_layer_nabla_a)
+        current_layer_nabla_a = last_layer_nabla_a
+
+        for l in range(self.L-1, 0, -1):
+            previous_layer_nabla_a = []
+            current_Z = self.weighted_sums[l]
+            current_W = self.weights[l]
+            previous_layer_nabla_a = np.dot(current_W.T, self.sigmoid_derivative(current_Z) * current_layer_nabla_a)
+            nabla_a.append(previous_layer_nabla_a)
+            current_layer_nabla_a = previous_layer_nabla_a
+        nabla_a.reverse()
+
+        nabla_b, nabla_w, current_layer_nabla_b, current_layer_nabla_w = [None], [None], [], []
+        for l in range(1, self.L):
+            current_layer_Z = self.weighted_sums[l]
+            A_prev = self.neuron_activations[l-1]
+            current_layer_nabla_b = self.sigmoid_derivative(current_layer_Z)*nabla_a[l]
+            current_layer_nabla_w = np.dot(current_layer_nabla_b, A_prev.T)
+            nabla_b.append(current_layer_nabla_b)
+            nabla_w.append(current_layer_nabla_w)
+        
+        return (nabla_w, nabla_b)
+    
+    def predict(self):
+        pass
+    
+    def evaluate(self):
+        pass
 
     def sigmoid_activation(self, Z):
         return 1/(1+np.exp(-Z))
@@ -54,83 +128,7 @@ class Network:
         return sum([np.dot(y[i]-h[i], y[i]-h[i]) for i in range(len(h))])/len(h)
 
     def cost_derivative(self, hi, yi):
-        # return 2/len(hi) * np.dot((yi - hi), -hi) 
-        # return 2*(yi-hi)*-hi
-        return hi-yi
-
-    def gradient_descent(self, epochs, mini_batch, learning_parameter, h , y):
-        nabla_wb = self.backprop(h, y)
-        nabla_w = nabla_wb[0]
-        nabla_b = nabla_wb[1]
-
-        for l in range(1, self.L):
-            self.weights[l] -= learning_parameter/len(mini_batch) * nabla_w[l]
-            self.biases[l] -= learning_parameter/len(mini_batch) * nabla_b[l]
-
-    def backprop(self, h, y):
-        nabla_a = []
-        nabla_aL = np.array([[self.cost_derivative(hi, yi)] for hi, yi in zip(h, y)])
-
-        nabla_a.append(nabla_aL)
-        nabla_aj = nabla_aL
-
-        for l in range(self.L-1, 0, -1):
-            nabla_ak = []
-            Z = self.weighted_sums[l]
-            W = self.weights[l]
-            nabla_ak = np.dot(W.T, self.sigmoid_derivative(Z) * nabla_aj)
-            nabla_a.append(nabla_ak)
-            nabla_aj = nabla_ak
-        nabla_a.reverse()
-
-        nabla_b, nabla_w, nabla_bj, nabla_wj = [None], [None], [], []
-        for l in range(1, self.L):
-            Z = self.weighted_sums[l]
-            W = self.weights[l]
-            A_prev = self.neuron_activations[l-1]
-            nabla_bj = self.sigmoid_derivative(Z)*nabla_a[l]
-            nabla_wj = np.dot(nabla_bj, A_prev.T)
-            nabla_b.append(nabla_bj)
-            nabla_w.append(nabla_wj)
-        
-        return (nabla_w, nabla_b)
-    
-    def predict(self, x):
-        pass
-    
-    def evaluate(self):
-        pass
-    
-    def train(self, activation_functions, X_train, y_train, X_val, y_val, epochs, mini_batch_size, learning_rate, flatten=False):
-
-        self.activation_functions = [None if func == None else self.activation_functions[func] for func in activation_functions]
-
-        for e in range(epochs):
-            training_data = list(zip(X_train, y_train))
-            shuffle(training_data)
-            h = None
-    
-            mini_batches = []
-            for mb in range(0, len(training_data), mini_batch_size):
-                mini_batches.append(training_data[mb:mb+mini_batch_size])
-
-            for mini_batch in mini_batches:
-                for x, y in mini_batch:
-                    self.forwardprop(x)
-                    h = self.neuron_activations[self.L-1].T[0]
-                    self.gradient_descent(epochs, mini_batch, learning_rate, h, y)
-
-            hs = []
-            for x, y in zip(X_train, y_train):
-                self.forwardprop(x)
-                h = self.neuron_activations[self.L-1].T[0]
-                hs.append(h)
-            predictions = np.array(hs).argmax(axis=1)
-            target_values = y_train.argmax(axis=1)
-            accuracy = sum([1 for i in range(len(hs)) if predictions[i] == target_values[i]])/len(hs)
-            cost = self.cost_function(hs, y_train)
-
-            print(f"Epoch {e} -> Cost: {cost}, Accuracy: {accuracy}")
+        return 2*(hi-yi)
 
 # def flatten_img(img):
 #     """Flattens one image"""
@@ -169,6 +167,4 @@ y_test_encoded = np.zeros((y_test.size, y_test.max()+1))
 y_test_encoded[np.arange(y_test.size), y_test] = 1
 
 net = Network((784, 16, 16, 10))
-
-# net.train((None, "sigmoid","sigmoid"), X_train_flattened[:1], y_train_encoded[:1], X_test_flattened[:1], y_test_encoded[:1], 1, 1, 0.5)
-net.train((None, "sigmoid", "sigmoid", "sigmoid"), X_train_flattened, y_train_encoded, X_test_flattened, y_test_encoded, 30, 10, 0.5)
+net.stochastic_gradient_descent((None, "sigmoid", "sigmoid", "sigmoid"), X_train_flattened, y_train_encoded, 30, 10, 0.5)
